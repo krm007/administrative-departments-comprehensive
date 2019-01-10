5350b5a88096234f982df93a2011f79ac86782c9
kkkkk <799902293@qq.com>
2019-01-10 11:02:46 +0800

    配置多表表头。

--- src/views/FinancialCollectingTable.tsx
+++ src/views/FinancialCollectingTable.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Tabs } from "antd";
-import NormalTableTitleConfig from "../config/NormalTableTitleConfig";
 import BocoTable from "./BocoTable";
+import MoreTableTitleConfig from "../config/MoreTableTitleConfig";
 
 /**
  * 描述：
@@ -16,68 +16,68 @@ class FinancialCollectingTable extends React.Component {
         <Tabs defaultActiveKey="2">
           <Tabs.TabPane tab={<span>在院病人费用汇总报表</span>} key="1">
             <BocoTable
-              title={"慢性病月度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"在院病人费用汇总报表"}
+              tableTitle={MoreTableTitleConfig.get("YuanBingQuFeiYong")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>出院病人费用统计表</span>} key="2">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"出院病人费用统计表"}
+              tableTitle={MoreTableTitleConfig.get("BingQuChuYuanYingShou")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>门诊医药费开单医生汇总</span>} key="3">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"门诊医药费开单医生汇总"}
+              tableTitle={MoreTableTitleConfig.get("MenZhenKeShiFeiYong")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>门诊医药费开单科室汇总表</span>} key="4">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"门诊医药费开单科室汇总表"}
+              tableTitle={MoreTableTitleConfig.get("MenZhenKaiDanKeShiFei")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>门诊日报汇总</span>} key="5">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"门诊日报汇总"}
+                tableTitle={MoreTableTitleConfig.get("RiBaoHuiZongChaXun")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>门诊清单汇总</span>} key="6">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"门诊清单汇总"}
+                tableTitle={MoreTableTitleConfig.get("MenZhenHuiZongQingDan")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>住院日报汇总</span>} key="7">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"住院日报汇总"}
+                tableTitle={MoreTableTitleConfig.get("ZhuYuanRiBao")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>住院清单汇总</span>} key="8">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"住院清单汇总"}
+                tableTitle={MoreTableTitleConfig.get("ZhuYuanQingDan")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>门诊挂账统计明细</span>} key="9">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"门诊挂账统计明细"}
+                tableTitle={MoreTableTitleConfig.get("MenZhenGuaZhangTongJi")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>门诊挂账统计单位汇总</span>} key="10">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"门诊挂账统计单位汇总"}
+                tableTitle={MoreTableTitleConfig.get("MenZhenGuaZhangHuiZong")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>住院挂账统计</span>} key="11">
             <BocoTable
-                title={"慢性病季度汇总表"}
-                tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+                title={"住院挂账统计"}
+                tableTitle={MoreTableTitleConfig.get("ZhuYuanHuoShiFeiGuaZhang")}
             />
           </Tabs.TabPane>
         </Tabs>
--- src/views/InHospitalFashionableTable.tsx
+++ src/views/InHospitalFashionableTable.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { Tabs } from "antd";
-import NormalTableTitleConfig from "../config/NormalTableTitleConfig";
 import BocoTable from "./BocoTable";
+import MoreTableTitleConfig from "../config/MoreTableTitleConfig";
 
 /**
  * 描述：
@@ -16,50 +16,50 @@ class InHospitalFashionableTable extends React.Component {
         <Tabs defaultActiveKey="2">
           <Tabs.TabPane tab={<span>出院病人费用统计表</span>} key="1">
             <BocoTable
-              title={"慢性病月度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"出院病人费用统计表"}
+              tableTitle={MoreTableTitleConfig.get("ChuYuanBingRenFeiYong")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>出院病人收入统计表</span>} key="2">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"出院病人收入统计表"}
+              tableTitle={MoreTableTitleConfig.get("ChuYuanBingRenShouRu")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>住院日报汇总</span>} key="3">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"住院日报汇总"}
+              tableTitle={MoreTableTitleConfig.get("ZhuYuanRiBao")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>住院清单汇总</span>} key="4">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"住院清单汇总"}
+              tableTitle={MoreTableTitleConfig.get("ZhuYuanQingDan")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>住院挂账统计</span>} key="5">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"住院挂账统计"}
+              tableTitle={MoreTableTitleConfig.get("ZhuYuanHuoShiFeiGuaZhang")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>出院病人结账汇总明细</span>} key="6">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"出院病人结账汇总明细"}
+              tableTitle={MoreTableTitleConfig.get("ChuYuanBingRenJieZhang")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>欠费查询</span>} key="7">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"欠费查询"}
+              tableTitle={MoreTableTitleConfig.get("QianFeiKongZhiChaXun")}
             />
           </Tabs.TabPane>
           <Tabs.TabPane tab={<span>医疗费申请核拨表</span>} key="8">
             <BocoTable
-              title={"慢性病季度汇总表"}
-              tableTitle={NormalTableTitleConfig.get("MenZhenKaiDanFeiYong")}
+              title={"医疗费申请核拨表"}
+              tableTitle={MoreTableTitleConfig.get("DingDianShenQingHeDui")}
             />
           </Tabs.TabPane>
         </Tabs>
