19775e74735f37e02e1a39f26ec96e305e4982ad
kkkkk <799902293@qq.com>
2019-01-11 16:31:29 +0800

    调整modal

--- src/views/InfectiousDiseaseStatisticsTable.tsx
+++ src/views/InfectiousDiseaseStatisticsTable.tsx
@@ -1,11 +1,12 @@
 import * as React from "react";
 import { createStyles, Theme, withStyles } from "@material-ui/core/styles";
 import { WithStyles } from "@material-ui/core/styles/withStyles";
-import { Tabs, Table, Row, Col } from "antd";
+import { Tabs, Table, Row, Col,Modal, Button } from "antd";
 import BocoTable from "./BocoTable";
 import MoreTableTitleConfig from "../config/MoreTableTitleConfig";
 import Pie from "./bizchart/Pie";
 import Bar from "./bizchart/Bar";
+import Line from "./bizchart/Line";
 
 const styles = (theme: Theme) =>
   createStyles({
@@ -14,7 +15,9 @@ const styles = (theme: Theme) =>
       padding: "20px"
     }
   });
-
+interface Istate{
+  visible?:boolean
+}
 interface Iprops extends WithStyles<typeof styles> {}
 /**
  * 描述：
@@ -22,7 +25,34 @@ interface Iprops extends WithStyles<typeof styles> {}
  * @author sunshixiong
  * @date 2019/1/9-10:16
  */
-class InfectiousDiseaseStatisticsTable extends React.Component<Iprops> {
+class InfectiousDiseaseStatisticsTable extends React.Component<Iprops,Istate> {
+  constructor(props:Iprops){
+    super(props);
+    this.state={
+      visible:false
+    }
+
+  }
+  public showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+
+  public handleOk = (e:any) => {
+    // console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
+  public handleCancel = (e:any) => {
+    // console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
+
   public render() {
     const { classes } = this.props;
     /** 表头 */
@@ -117,6 +147,109 @@ class InfectiousDiseaseStatisticsTable extends React.Component<Iprops> {
     ];
     /** 分组柱状图的x轴显示字段 */
     const barXAxis = ["肺结核", "HIV阳性", "病毒性肝炎", "梅毒", "淋病"];
+    /** 折线图数据 */
+    const lineData = [
+      {
+        item: "2014",
+        一季度: "2",
+        二季度: "5",
+        三季度: "4",
+        四季度: "3"
+      },{
+        item: "2015",
+        一季度: "0",
+        二季度: "5",
+        三季度: "3",
+        四季度: "1"
+      },{
+        item: "2016",
+        一季度: "5",
+        二季度: "3",
+        三季度: "4",
+        四季度: "6"
+      },{
+        item: "2017",
+        一季度: "4",
+        二季度: "1",
+        三季度: "5",
+        四季度: "6"
+      },{
+        item: "2018",
+        一季度: "2",
+        二季度: "4",
+        三季度: "6",
+        四季度: "6"
+      }
+    ];
+    /** modal数据 */
+    const ModalColumns: any[] = [
+      {
+        title: "",
+        dataIndex: "name"
+      },
+      {
+        title: "2014",
+        dataIndex: "bing_1"
+      },
+      {
+        title: "2015",
+        dataIndex: "bing_2"
+      },
+      {
+        title: "2016",
+        dataIndex: "bing_3"
+      },
+      {
+        title: "2017",
+        dataIndex: "bing_4"
+      },
+      {
+        title: "2018",
+        dataIndex: "bing_5"
+      }
+    ];
+    const ModalData: any[] = [
+      {
+        key: "1",
+        name: "第一季度",
+        bing_1: "170",
+        bing_2: "100",
+        bing_3: "160",
+        bing_4: "170",
+        bing_5: "100",
+
+      },
+      {
+        key: "2",
+        name: "第二季度",
+        bing_1: "170",
+        bing_2: "100",
+        bing_3: "160",
+        bing_4: "170",
+        bing_5: "100",
+
+      },
+      {
+        key: "3",
+        name: "第三季度",
+        bing_1: "170",
+        bing_2: "100",
+        bing_3: "160",
+        bing_4: "170",
+        bing_5: "100",
+
+      },
+      {
+        key: "4",
+        name: "第4季度",
+        bing_1: "170",
+        bing_2: "100",
+        bing_3: "160",
+        bing_4: "170",
+        bing_5: "100",
+
+      }
+    ];
     return (
       <div className={classes.root}>
         <Tabs defaultActiveKey="2">
@@ -134,9 +267,58 @@ class InfectiousDiseaseStatisticsTable extends React.Component<Iprops> {
               tableTitle={MoreTableTitleConfig.get("FaDingChuanRanFaBing")}
               month={true}
             />
+            <Button type="primary" onClick={this.showModal} style={{margin:"20px"}}>
+              表头触发
+            </Button>
+            <Modal
+                // title="Basic Modal"
+                visible={this.state.visible}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+                width={"70vw"}
+            >
+              <div style={{ padding: "20px" }}>
+                <Row>
+                  <Col span={20} offset={2}>
+                    <Table
+                        columns={ModalColumns}
+                        size="middle"
+                        dataSource={ModalData}
+                        pagination={false}
+                        bordered={true}
+                        title={() => (
+                            <span style={{ padding: "10px" }}>
+                                <img
+                                    src={require("../images/chartIcon.png")}
+                                    alt=""
+                                    style={{
+                                      float: "left",
+                                      marginLeft: "1vw",
+                                      marginRight: "0.5vw"
+                                    }}
+                                />
+                                附表：近5年发病情况
+                              </span>
+                        )}
+                    />
+                  </Col>
+                </Row>
+              </div>
+              <div style={{ padding: "20px" }}>
+                <Row>
+                  <Col span={20} offset={2}>
+                    <Line
+                        titleChart={"报告发病情况"}
+                        chartData={lineData}
+                        lineXAxis={["一季度","二季度","三季度","四季度"]}
+                    />
+                  </Col>
+                </Row>
+              </div>
+            </Modal>
             <div className={classes.myChart}>
               <Row>
-                <Col span={11}>
+                <Col span={16} offset={4}>
                   <Pie
                     titleChart={"法定传染病发情况比例图"}
                     chartData={PieData}
